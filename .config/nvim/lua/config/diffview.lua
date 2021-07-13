daa950924e7e22129da3c6303b3b2dd779c28800
CristoAMH <cristoamh@gmail.com>
2021-07-13 12:14:07 +0100

    add diffview keybinds

---
+++ .config/nvim/lua/config/diffview.lua
@@ -0,0 +1,4 @@
+local utils = require('utils')
+
+utils.map('n', '<leader>ty', '<cmd>tabn<cr>')
+utils.map('n', '<leader>tr', '<cmd>tabp<cr>')
--- .config/nvim/plugin/packer_compiled.lua
+++ .config/nvim/plugin/packer_compiled.lua
@@ -44,8 +44,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/Users/cristoamh/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/Users/cristoamh/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/Users/cristoamh/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/Users/cristoamh/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/Users/cristoamh/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
+local package_path_str = "/home/cristoamh/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/home/cristoamh/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/home/cristoamh/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/home/cristoamh/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/home/cristoamh/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -71,121 +71,125 @@ time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
   ["completion-nvim"] = {
     loaded = true,
-    path = "/Users/cristoamh/.local/share/nvim/site/pack/packer/start/completion-nvim"
+    path = "/home/cristoamh/.local/share/nvim/site/pack/packer/start/completion-nvim"
   },
   ["dashboard-nvim"] = {
     loaded = true,
-    path = "/Users/cristoamh/.local/share/nvim/site/pack/packer/start/dashboard-nvim"
+    path = "/home/cristoamh/.local/share/nvim/site/pack/packer/start/dashboard-nvim"
+  },
+  ["diffview.nvim"] = {
+    loaded = true,
+    path = "/home/cristoamh/.local/share/nvim/site/pack/packer/start/diffview.nvim"
   },
   ["galaxyline.nvim"] = {
     loaded = true,
-    path = "/Users/cristoamh/.local/share/nvim/site/pack/packer/start/galaxyline.nvim"
+    path = "/home/cristoamh/.local/share/nvim/site/pack/packer/start/galaxyline.nvim"
   },
   ["git-messenger.vim"] = {
     loaded = true,
-    path = "/Users/cristoamh/.local/share/nvim/site/pack/packer/start/git-messenger.vim"
+    path = "/home/cristoamh/.local/share/nvim/site/pack/packer/start/git-messenger.vim"
   },
   ["gitsigns.nvim"] = {
     config = { "\27LJ\2\n>\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\vconfig\20config.gitsigns\frequire\0" },
     loaded = false,
     needs_bufread = false,
-    path = "/Users/cristoamh/.local/share/nvim/site/pack/packer/opt/gitsigns.nvim"
+    path = "/home/cristoamh/.local/share/nvim/site/pack/packer/opt/gitsigns.nvim"
   },
   ["indent-blankline.nvim"] = {
     loaded = true,
-    path = "/Users/cristoamh/.local/share/nvim/site/pack/packer/start/indent-blankline.nvim"
+    path = "/home/cristoamh/.local/share/nvim/site/pack/packer/start/indent-blankline.nvim"
   },
   kommentary = {
     loaded = true,
-    path = "/Users/cristoamh/.local/share/nvim/site/pack/packer/start/kommentary"
+    path = "/home/cristoamh/.local/share/nvim/site/pack/packer/start/kommentary"
   },
   ["lspsaga.nvim"] = {
     loaded = true,
-    path = "/Users/cristoamh/.local/share/nvim/site/pack/packer/start/lspsaga.nvim"
+    path = "/home/cristoamh/.local/share/nvim/site/pack/packer/start/lspsaga.nvim"
   },
   ["neoscroll.nvim"] = {
     config = { "require('config.scroll')" },
     keys = { { "", "<C-u>" }, { "", "<C-d>" }, { "", "C-b" }, { "", "gg" }, { "", "G" } },
     loaded = false,
     needs_bufread = false,
-    path = "/Users/cristoamh/.local/share/nvim/site/pack/packer/opt/neoscroll.nvim"
+    path = "/home/cristoamh/.local/share/nvim/site/pack/packer/opt/neoscroll.nvim"
   },
   ["nlua.nvim"] = {
     loaded = true,
-    path = "/Users/cristoamh/.local/share/nvim/site/pack/packer/start/nlua.nvim"
+    path = "/home/cristoamh/.local/share/nvim/site/pack/packer/start/nlua.nvim"
   },
   ["nvim-lspconfig"] = {
     loaded = true,
-    path = "/Users/cristoamh/.local/share/nvim/site/pack/packer/start/nvim-lspconfig"
+    path = "/home/cristoamh/.local/share/nvim/site/pack/packer/start/nvim-lspconfig"
   },
   ["nvim-lspinstall"] = {
     loaded = true,
-    path = "/Users/cristoamh/.local/share/nvim/site/pack/packer/start/nvim-lspinstall"
+    path = "/home/cristoamh/.local/share/nvim/site/pack/packer/start/nvim-lspinstall"
   },
   ["nvim-tree.lua"] = {
     loaded = true,
-    path = "/Users/cristoamh/.local/share/nvim/site/pack/packer/start/nvim-tree.lua"
+    path = "/home/cristoamh/.local/share/nvim/site/pack/packer/start/nvim-tree.lua"
   },
   ["nvim-treesitter"] = {
     loaded = true,
-    path = "/Users/cristoamh/.local/share/nvim/site/pack/packer/start/nvim-treesitter"
+    path = "/home/cristoamh/.local/share/nvim/site/pack/packer/start/nvim-treesitter"
   },
   ["nvim-ts-autotag"] = {
     loaded = true,
-    path = "/Users/cristoamh/.local/share/nvim/site/pack/packer/start/nvim-ts-autotag"
+    path = "/home/cristoamh/.local/share/nvim/site/pack/packer/start/nvim-ts-autotag"
   },
   ["nvim-ts-context-commentstring"] = {
     loaded = true,
-    path = "/Users/cristoamh/.local/share/nvim/site/pack/packer/start/nvim-ts-context-commentstring"
+    path = "/home/cristoamh/.local/share/nvim/site/pack/packer/start/nvim-ts-context-commentstring"
   },
   ["nvim-web-devicons"] = {
     loaded = true,
-    path = "/Users/cristoamh/.local/share/nvim/site/pack/packer/start/nvim-web-devicons"
+    path = "/home/cristoamh/.local/share/nvim/site/pack/packer/start/nvim-web-devicons"
   },
   ["packer.nvim"] = {
     loaded = false,
     needs_bufread = false,
-    path = "/Users/cristoamh/.local/share/nvim/site/pack/packer/opt/packer.nvim"
+    path = "/home/cristoamh/.local/share/nvim/site/pack/packer/opt/packer.nvim"
   },
   ["plenary.nvim"] = {
     loaded = true,
-    path = "/Users/cristoamh/.local/share/nvim/site/pack/packer/start/plenary.nvim"
+    path = "/home/cristoamh/.local/share/nvim/site/pack/packer/start/plenary.nvim"
   },
   ["popup.nvim"] = {
     loaded = true,
-    path = "/Users/cristoamh/.local/share/nvim/site/pack/packer/start/popup.nvim"
+    path = "/home/cristoamh/.local/share/nvim/site/pack/packer/start/popup.nvim"
   },
   ["startuptime.vim"] = {
     commands = { "StartupTime" },
     loaded = false,
     needs_bufread = false,
-    path = "/Users/cristoamh/.local/share/nvim/site/pack/packer/opt/startuptime.vim"
+    path = "/home/cristoamh/.local/share/nvim/site/pack/packer/opt/startuptime.vim"
   },
   ["telescope-fzy-native.nvim"] = {
     loaded = true,
-    path = "/Users/cristoamh/.local/share/nvim/site/pack/packer/start/telescope-fzy-native.nvim"
+    path = "/home/cristoamh/.local/share/nvim/site/pack/packer/start/telescope-fzy-native.nvim"
   },
   ["telescope.nvim"] = {
     loaded = true,
-    path = "/Users/cristoamh/.local/share/nvim/site/pack/packer/start/telescope.nvim"
+    path = "/home/cristoamh/.local/share/nvim/site/pack/packer/start/telescope.nvim"
   },
   ["tokyonight.nvim"] = {
     loaded = true,
-    path = "/Users/cristoamh/.local/share/nvim/site/pack/packer/start/tokyonight.nvim"
+    path = "/home/cristoamh/.local/share/nvim/site/pack/packer/start/tokyonight.nvim"
   },
   ultisnips = {
     loaded = true,
-    path = "/Users/cristoamh/.local/share/nvim/site/pack/packer/start/ultisnips"
+    path = "/home/cristoamh/.local/share/nvim/site/pack/packer/start/ultisnips"
   },
   ["vim-matchup"] = {
-    after_files = { "/Users/cristoamh/.local/share/nvim/site/pack/packer/opt/vim-matchup/after/plugin/matchit.vim" },
+    after_files = { "/home/cristoamh/.local/share/nvim/site/pack/packer/opt/vim-matchup/after/plugin/matchit.vim" },
     loaded = false,
     needs_bufread = true,
-    path = "/Users/cristoamh/.local/share/nvim/site/pack/packer/opt/vim-matchup"
+    path = "/home/cristoamh/.local/share/nvim/site/pack/packer/opt/vim-matchup"
   },
   ["vim-snippets"] = {
     loaded = true,
-    path = "/Users/cristoamh/.local/share/nvim/site/pack/packer/start/vim-snippets"
+    path = "/home/cristoamh/.local/share/nvim/site/pack/packer/start/vim-snippets"
   }
 }
 
@@ -200,9 +204,9 @@ time([[Defining lazy-load commands]], false)
 time([[Defining lazy-load keymaps]], true)
 vim.cmd [[noremap <silent> gg <cmd>lua require("packer.load")({'neoscroll.nvim'}, { keys = "gg", prefix = "" }, _G.packer_plugins)<cr>]]
 vim.cmd [[noremap <silent> G <cmd>lua require("packer.load")({'neoscroll.nvim'}, { keys = "G", prefix = "" }, _G.packer_plugins)<cr>]]
+vim.cmd [[noremap <silent> <C-d> <cmd>lua require("packer.load")({'neoscroll.nvim'}, { keys = "<lt>C-d>", prefix = "" }, _G.packer_plugins)<cr>]]
 vim.cmd [[noremap <silent> <C-u> <cmd>lua require("packer.load")({'neoscroll.nvim'}, { keys = "<lt>C-u>", prefix = "" }, _G.packer_plugins)<cr>]]
 vim.cmd [[noremap <silent> C-b <cmd>lua require("packer.load")({'neoscroll.nvim'}, { keys = "C-b", prefix = "" }, _G.packer_plugins)<cr>]]
-vim.cmd [[noremap <silent> <C-d> <cmd>lua require("packer.load")({'neoscroll.nvim'}, { keys = "<lt>C-d>", prefix = "" }, _G.packer_plugins)<cr>]]
 time([[Defining lazy-load keymaps]], false)
 
 vim.cmd [[augroup packer_load_aucmds]]
